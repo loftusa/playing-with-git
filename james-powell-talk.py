b1fea850647e0160bb18a56b981320aaac65421d
Alex Loftus <alexloftus2004@gmail.com>
2018-10-21 12:13:16 -0400

    Updated powell talk

--- james-powell-talk.py
+++ james-powell-talk.py
@@ -1,20 +1,22 @@
 # dec.py
 
 """
-0. run python file in terminal, open as environment
-1. use dis to get information about function
-2. look at byte code for function
-3. look at default arguments for function
-4. look at name of function
+0. run python file in console, open as environment
+1. use dis to get information about method
+2. look at byte code for method
+3. look at default arguments for method
+4. look at name of method
 
 """
 
-#%%
-# foo is a decorator which will add ', fucking.' to the sentence created by bar.
-# bar is a function that will return "Hi, my name is x".
-def foo(fn):
+# %%
+# foo is a decorator which will add ', heckin.' to the sentence created by bar.
+# bar is a method that will return "Hi, my name is x".
+
+
+def foo(func):
     def f(*args, **kwargs):
-        return fn(*args, **kwargs) + ", fucking."
+        return func(*args, **kwargs) + ", heckin."
     return f
 
 
@@ -22,22 +24,24 @@ def foo(fn):
 def bar(x):
     return "Hi, my name is {}".format(x)
 
+
 @foo
 def biff(x):
     return str(x)
 
+
 def print_goals():
     print("""
-    0. run python file in terminal, open as environment
-    1. use dis to get information about function
-    2. look at byte code for function
-    3. look at default arguments for function
-    4. look at name of function
+    0. run python file in console, open as environment
+    1. use dis to get information about method. This will tell you the exact stuff that the method is doing.
+    2. look at byte code for method
+    3. look at default arguments for method
+    4. look at name of method
 
         """)
 
-print(bar("Alex"))
-print(biff(31))
-print_goals()
-
 #%%
+def testing_out_syntax(num1: int, num2: int, st: str) -> str:
+   return str('num1 + num2 = {} '.format(num1 + num2) + ', and the string is {}'.format(st))
+   
+print(testing_out_syntax(1, 2, 5))
---
+++ plotting-tes.py
@@ -0,0 +1,11 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#%%
+x = np.full()
+print(x)
+
+
+#%%
